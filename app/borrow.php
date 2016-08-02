2990819b2aa64bd054f9d73a1d5ed7f28b22c46c
oohcode <oohcode@gmail.com>
2016-08-02 11:33:11 +0800

    add borrow table

--- app/borrow.php
+++ app/borrow.php
@@ -1,8 +1,33 @@
 <div id="content">
-<form name="input" action="     " method="get">
-Student ID:
-<input type="text" name="
-</div>
+<div id="pad">
+<table border="1">
+<tbody>
+<tr>
+<th >编号</th>
+<th >标题</th>
+<th>作者</th>
+<th>操作</th>
+</tr>
 
-
-
+<tr>
+<td>1</td>
+<td>C语言</td>
+<td>谭浩强</td>
+<td><button>借书</button></td>
+</tr>
+<tr>
+<td>2</td>
+<td>C++语言</td>
+<td>谭浩强</td>
+<td><button>借书</button></td>
+</tr>
+<tr>
+<td>3</td>
+<td>PHP语言</td>
+<td>鸟哥</td>
+<td><button>借书</button></td>
+</tr>
+</tbody>
+</table>
+<div>
+<div>
