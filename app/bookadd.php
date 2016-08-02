b13dacc158614204a98f3aeed8f5f0fc1c31bc1d
eggchai <cxk335096145@163.com>
2016-08-02 15:40:27 +0800

    something

--- app/bookadd.php
+++ app/bookadd.php
@@ -1,8 +1,28 @@
-<?php include("./head2.php") ?>
 <div id="content">
-<form name="input" action="     " method="get">
-Student ID:
-<input type="text" name="
+<div id="pad">
+<h4>填写添加图书信息</h4>
+<br />
+<table border="1">
+<tr>
+ <td>书名</td>
+ <td><input type="text" name="bookname" /></td>
+</tr>
+<tr>
+ <td>作者</td>
+ <td><input type="text" name="writer" /></td>
+</tr>
+<tr>
+ <td>出版社</td>
+ <td><input type="text" name="press" /></td>
+</tr>
+<tr>
+　<td>类别</td>
+ <td><input type="text" name="type" /></td>
+</tr>
+<tr>
+ <td>本数</td>
+ <td><input type="text" name="num" /></td>
+<br />
+<input type="submit" value="确认提交" />
 </div>
-
-
+</div>
